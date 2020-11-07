107966897f94fe21b23c2b5dc93fde44b68e2831
Jasper Voss <jaspervoss812@gmail.com>
2020-11-07 10:22:47 -0500

    motors

--- Scripts/motorClass.py
+++ Scripts/motorClass.py
@@ -44,6 +44,9 @@ class Motor:
             self.output[i+1] = self.step_seq[self.curr_step0][i]
         for i in self.output:
             out = out + i
+        print(out)
+        print(self.curr_step0)
+        print('')
         mcp.output(out)
                 
     def move_step1(self, direction):
