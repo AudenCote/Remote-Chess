30b80284cebd8f4d80a8d8a5967049332e2148e6
Jasper Voss <jaspervoss812@gmail.com>
2020-11-06 20:07:48 -0500

    ug

--- Scripts/hookupMotors.py
+++ Scripts/hookupMotors.py
@@ -1,7 +1,14 @@
 import move
+from PEC import MCP
 
 for i in range(0, 4):
 	move.off(i)
 
+mcp = MCP()
+
 while True:
-	move.manual(int(input("motor?: ")), int(input("steps?: ")))
+	inputtext = input("Custom output (enter to break): ")
+	if inputtext == "":
+		break
+	else:
+		mcp.out(inputtext)
