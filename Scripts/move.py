2fed9bc2cdcf0a146b682be804d1786170827fe1
Jasper Voss <jaspervoss812@gmail.com>
2020-11-07 09:58:25 -0500

    motors

--- Scripts/move.py
+++ Scripts/move.py
@@ -30,7 +30,7 @@ def manual(motor, steps):
     if motor == 0:
         for i in range(abs(steps)):
             a_motors.move_step0(abs(steps)/steps)
-            time.sleep(.002)
+            time.sleep(.005)
     if motor == 1:
         for i in range(abs(steps)):
             a_motors.move_step1(abs(steps)/steps)
